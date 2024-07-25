630fcb7b5bf9a73c5a75b8d36c84d9b1c7f4ac84
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2024-07-25 12:45:31 +0100

    method injection 1

--- src/main/java/com/dadi/springbootcourse/ApplicationConfig.java
+++ src/main/java/com/dadi/springbootcourse/ApplicationConfig.java
@@ -9,13 +9,11 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
 
     @Bean
-    //@Qualifier("first")
     public MyFirstClass myFirstBean(){
         return new MyFirstClass("My First Bean");
     }
 
     @Bean
-    //@Qualifier("second")
     public MyFirstClass mySecondBean(){
         return new MyFirstClass("My Second Bean");
     }
--- src/main/java/com/dadi/springbootcourse/MyFirstService.java
+++ src/main/java/com/dadi/springbootcourse/MyFirstService.java
@@ -1,23 +1,23 @@
 package com.dadi.springbootcourse;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-    private final MyFirstClass myFirstClass2;
-    private final MyFirstClass myFirstClass;
+    //private final MyFirstClass myFirstClass2;
+    private  MyFirstClass myFirstClass;
 
-    public MyFirstService(@Qualifier("myFirstBean") MyFirstClass myFirstClass,@Qualifier("mySecondBean") MyFirstClass myFirstClass2) {
+    @Autowired
+    public void injectDependencies(@Qualifier("myFirstBean") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
-        this.myFirstClass2= myFirstClass2;
     }
 
 
     public String tellAStory(){
-        return "the dependency is saying : "+ myFirstClass.sayHello() + "\n" +
-         "the dependency is saying : "+ myFirstClass2.sayHello();
+        return "the dependency is saying : "+ myFirstClass.sayHello();
     }
 }
