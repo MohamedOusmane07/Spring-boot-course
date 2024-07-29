80fa563340bed6746c1b2512a66f26d8bd722644
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2024-07-29 11:53:04 +0100

    First Controller

---
+++ src/main/java/com/dadi/springbootcourse/OrderRecord.java
@@ -0,0 +1,7 @@
+package com.dadi.springbootcourse;
+
+public record OrderRecord(
+        int id,
+         String name,
+         String profession) {
+}
--- src/main/java/com/dadi/springbootcourse/SpringbootCourseApplication.java
+++ src/main/java/com/dadi/springbootcourse/SpringbootCourseApplication.java
@@ -6,23 +6,16 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
 
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class SpringbootCourseApplication {
 
     public static void main(String[] args) {
+        run(SpringbootCourseApplication.class,args);
 
-        var app= new SpringApplication(SpringbootCourseApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-        var ctx=app.run(args);
 
-       MyFirstService myFirstService = ctx.getBean("myFirstService",MyFirstService.class);
-        System.out.println(myFirstService.tellAStory());
-        System.out.println(myFirstService.getCustomProperty());
-        System.out.println(myFirstService.getCustomProperty2());
-        System.out.println(myFirstService.getCustomPropertyInt());
-        System.out.println(myFirstService.getCustomPropertyInt2());
-        //System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
-        //System.out.println(myFirstService.getCustomPropertyFromAnotherFile2());
+
 
     }
 
